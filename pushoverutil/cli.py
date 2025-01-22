f07d2c8930d7cf39ca5acd10f268b4fb1f3b67e2
chief <1092786+chief8192@users.noreply.github.com>
2025-01-22 08:20:32 -0500

    Adds helper CLI module

--- pushoverutil/cli.py
+++ pushoverutil/cli.py
@@ -23,19 +23,38 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from setuptools import setup
+import argparse
 
-setup(
-    name="pushover_util",
-    version="1.1.0",
-    description="Python-based helper library for interacting with the Pushover API.",
-    url="https://github.com/chief8192/pushover-util",
-    author="Matt Doyle",
-    license="MIT",
-    packages=["pushoverutil"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-    ],
-)
+from push import Push, Priority
+
+
+def main():
+
+    # Parse the command line arguments into a context.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--user_key", dest="user_key", required=True)
+    parser.add_argument("--api_token", dest="api_token", required=True)
+    parser.add_argument("--message", dest="message", required=True)
+    parser.add_argument(
+        "--priority",
+        dest="priority",
+        action="store_true",
+        required=False,
+        default=False,
+    )
+    parser.add_argument("--title", dest="title", required=False, default=None)
+    context = parser.parse_args()
+
+    priority = Priority.HIGH if context.priority else Priority.NORMAL
+
+    Push(
+        context.user_key,
+        context.api_token,
+        context.message,
+        priority=priority,
+        title=context.title,
+    )
+
+
+if __name__ == "__main__":
+    main()
